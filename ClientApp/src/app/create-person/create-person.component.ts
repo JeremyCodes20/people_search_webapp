9dfeba55d2f70db4d5db12c1886c187ce33710d0
Jeremy Young <young.a.jeremy@gmail.com>
2021-04-22 10:53:33 -0600

    Successful creation of a new person now navigates to that person's detail page

--- ClientApp/src/app/create-person/create-person.component.ts
+++ ClientApp/src/app/create-person/create-person.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Inject, forwardRef } from '@angular/core';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 import { Person } from '../person';
 
 import { PersonService } from '../person.service';
+
 
 @Component({
   selector: 'app-create-person',
@@ -21,14 +23,20 @@ export class CreatePersonComponent implements OnInit {
   };
 
   constructor(private location: Location,
+    private router: Router,
     @Inject(forwardRef(() => PersonService)) private personService: PersonService) { }
 
   ngOnInit(): void {
   }
 
   createPerson(): void {
+    const observePost = {
+      next: person => this.person = person,
+      error: err => console.error('Error occurred during POST: ' + err),
+      complete: () => this.router.navigate([`detail/${this.person.id}`])
+    };
     this.personService.createPerson(this.person)
-      .subscribe(person => this.person = person);
+      .subscribe(observePost);
     // this.location.back();
   }
 
