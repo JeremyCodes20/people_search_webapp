799668815b457182bbd01f23402f04f136bd9320
Jeremy Young <young.a.jeremy@gmail.com>
2021-04-23 10:42:19 -0600

    Cleaned up personservice

--- ClientApp/src/app/person.service.ts
+++ ClientApp/src/app/person.service.ts
@@ -28,28 +28,7 @@ export class PersonService {
 
   createPerson(person: Person): Observable<Person> {
     delete person.id;
-    const httpOptions = {
-      headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-      })
-    };
-    console.log(`${person.fName} ${person.lName}`);
+  
     return this.http.post<Person>(this.PEOPLE_API, person);
   }
-
-//   private handleError(error: HttpErrorResponse) {
-//   if (error.error instanceof ErrorEvent) {
-//     // A client-side or network error occurred. Handle it accordingly.
-//     console.error('An error occurred:', error.error.message);
-//   } else {
-//     // The backend returned an unsuccessful response code.
-//     // The response body may contain clues as to what went wrong.
-//     console.error(
-//       `Backend returned code ${error.status}, ` +
-//       `body was: ${error.error}`);
-//   }
-//   // Return an observable with a user-facing error message.
-//   return throwError(
-//     'Something bad happened; please try again later.');
-// }
 }
